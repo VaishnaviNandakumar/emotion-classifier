0eef30968e499af7cd389cb7ad8ce0b4daddd4de
Vaishnavi Nandakumar <vaishoo.nk@gmail.com>
2020-04-24 22:17:00 +0530

    Modified conversion file

--- Dataset/conversion.py
+++ Dataset/conversion.py
@@ -1,41 +1,18 @@
 import cv2 
 import os 
-  
+import math 
 # Read the video from specified path 
-cam = cv2.VideoCapture("C:\\Users\\ACER\\Desktop\\EmotionDetection\\Dataset\\TrainCartoon.mp4") 
-  
-try: 
-      
-    # creating a folder named data 
-    if not os.path.exists('images'): 
-        os.makedirs('images') 
-  
-# if not created then raise error 
-except OSError: 
-    print ('Error: Creating directory of data') 
-  
-# frame 
-currentframe = 0
-  
-while(True): 
-      
-    # reading from frame 
-    ret,frame = cam.read() 
-  
-    if ret: 
-        # if video is still left continue creating images 
-        name = './images/frame' + str(currentframe) + '.jpg'
-        print ('Creating...' + name) 
-  
-        # writing the extracted images 
-        cv2.imwrite(name, frame) 
-  
-        # increasing counter so that it will 
-        # show how many frames are created 
-        currentframe += 1
-    else: 
+count = 0
+videoFile = "C:\\Users\\ACER\\Desktop\\EmotionDetection\\Dataset\\TestCartoon.mp4"
+cap = cv2.VideoCapture(videoFile) # capturing the video from the given path
+frameRate = cap.get(5) #frame rate
+x=1
+while(cap.isOpened()):
+    frameId = cap.get(1) #current frame number
+    ret, frame = cap.read()
+    if (ret != True):
         break
-  
-# Release all space and windows once done 
-cam.release() 
-cv2.destroyAllWindows() 
+    if (frameId % math.floor(frameRate) == 0):
+        filename ="test%d.jpg" % count;    count+=1
+        cv2.imwrite(filename, frame)
+cap.release()
